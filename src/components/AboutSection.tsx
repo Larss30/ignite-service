2af53177d130da0ec49edcadb0153b046211f145
Larss830 <larence830@gmail.com>
2025-08-24 16:02:06 +0800

    Update AboutSection.tsx

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -72,7 +72,7 @@ export default function AboutSection() {
           </div>
 
           {/* Features Grid */}
-          <div className="grid grid-cols-2 gap-6 sm:grid-cols-12">
+          <div className="grid grid-cols-1 gap-6">
             {features.map((feature, index) => (
               <div 
                 key={index}
